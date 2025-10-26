14f27dda4cae5b110b34d5063ed38e455f92f01a
Nabi94 <nebi9447@gmail.com>
2025-10-26 20:59:06 +0000

    new listener

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,13 +1,15 @@
+import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
 
-
+@Listeners({AllureTestNg.class})
 public class SimpleTest {
     WebDriver driver;
 
